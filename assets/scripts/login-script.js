ae4ac67070ed838f633a1ab326c49a205fb41ca2
FigoRepository <125849418+FigoRepository@users.noreply.github.com>
2023-02-20 11:39:14 +0700

    Update login-script.js
    Menambahkan komentar dengan format JSDocs

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,11 +5,35 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/** @module Login-Script*/
+/**
+* Membuat tampilan formulir dengan mendefenisikan variabel loginFormElement
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+/**
+* Membuat tampilan input email dengan mendefenisikan variabel inputEmailElement
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/**
+* Membuat tampilan input password dengan mendefenisikan variabel inputPasswordElement
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/**
+* expectedEmail sebagai email validasi
+* @constant {string}
+*/
 const expectedEmail = 'admin@dicoding.com';
+
+/**
+* expectedPassword sebagai password validasi
+* @constant {string}
+*/
 const expectedPassword = 'superpassword';
 
 loginFormElement.addEventListener('submit', function(event) {
